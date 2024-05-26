796ef4d2e5881d4d8ff43a88e89f1d0ac463bcfa
meannsenrealwat <meann.sen@realwat.net>
2024-05-26 09:08:42 -0600

    update git action

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! src/main.rs
 use zero2prod::run;
 #[tokio::main]
-async fn main() -> std::io::Result<()>{
-  run("127.0.0.1:8000")?.await
+async fn main() -> std::io::Result<()> {
+    run("127.0.0.1:8000")?.await
 }

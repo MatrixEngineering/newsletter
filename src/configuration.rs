e1c236cafea2fc4a31a59114ebdebb2b00954129
meannsenrealwat <meann.sen@realwat.net>
2024-05-28 11:41:42 +0700

    Update database configuration

--- src/configuration.rs
+++ src/configuration.rs
@@ -16,14 +16,14 @@ pub struct DatabaseSettings {
 impl DatabaseSettings {
     pub fn connection_string(&self) -> String {
         format!(
-            "postgres://{}:{}@{}:{}",
-            self.username, self.password, self.host, self.port
+            "postgres://{}:{}@{}:{}/{}",
+            self.username, self.password, self.host, self.port, self.database_name
         )
     }
 }
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
-    let mut settings = config::Config::builder()
+    let settings = config::Config::builder()
         .add_source(config::File::new(
             "configuration.yaml",
             config::FileFormat::Yaml,
--- src/lib.rs
+++ src/lib.rs
@@ -1,34 +1,3 @@
-//! src/lib.rs
-
 pub mod configuration;
-mod routes;
-mod startup;
-
-use actix_web::dev::Server;
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use serde::Deserialize;
-use std::net::TcpListener;
-
-#[derive(Deserialize)]
-#[allow(unused)]
-struct FormData {
-    name: String,
-    email: String,
-}
-
-async fn health_check(_req: HttpRequest) -> impl Responder {
-    HttpResponse::Ok()
-}
-async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
-    HttpResponse::Ok().finish()
-}
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
+pub mod routes;
+pub mod startup;

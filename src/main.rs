fd8374b932f2903be20c1005d1fcaca5b093a10e
meannsenrealwat <meann.sen@realwat.net>
2024-05-30 04:10:08 -0600

    Update configuration

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 //! src/main.rs
 use secrecy::ExposeSecret;
-use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -16,9 +16,9 @@ async fn main() -> std::io::Result<()> {
         "{}:{}",
         configuration.application.host, configuration.application.port
     );
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to postgres.");
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+        .connect_lazy(&configuration.database.connection_string().expose_secret());
 
     let listener = std::net::TcpListener::bind(address).expect("Failed to bind address Port.");
     run(listener, connection_pool)?.await
--- src/startup.rs
+++ src/startup.rs
@@ -1,11 +1,11 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgPool;
+use sqlx::{Error, Pool, Postgres};
 use std::net::TcpListener;
 use tracing_actix_web::TracingLogger;
 
-pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+pub fn run(listener: TcpListener, db_pool: Result<Pool<Postgres>, Error>) -> Result<Server, std::io::Error> {
     let connection = web::Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()

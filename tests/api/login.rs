bf20f5b6a7266e06499dd4b7494102619760aa8e
senmeannrealwat <meann.sen@realwat.net>
2024-08-23 06:47:05 -0600

    replace Cookies with flash message error authetication

--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,5 +1,3 @@
-use std::collections::HashSet;
-
 use crate::helpers::{assert_is_redirect_to, spawn_app};
 
 #[tokio::test]
@@ -14,16 +12,9 @@ async fn an_error_flash_message_is_set_on_failure() {
     let response = app.post_login(&login_body).await;
     assert_is_redirect_to(&response, "/login");
 
-    let cookies: HashSet<_> = response
-        .headers()
-        .get_all("Set-Cookie")
-        .into_iter()
-        .collect();
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains("<p><i>Authentication failed</i></p>"));
 
-    assert!(cookies.contains(
-        &reqwest::header::HeaderValue::from_str("_flash=Authentication failed").unwrap()
-    ));
-
-    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
-    assert_eq!(flash_cookie.value(), "Authentication failed")
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains("<p><i>Authentication failed</i></p>"));
 }

8b37a2698bf9f22d584cdbe7622cecd0c0b07e52
senmeannrealwat <meann.sen@realwat.net>
2024-08-23 07:54:15 -0600

    Fix CL Test error

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use zero2prod::startup::Application;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> Result<(), anyhow::Error> {
     let subscriber = get_subscriber("newsletter".into(), "info".into(), std::io::stdout);
     init_subscriber(subscriber);
 
